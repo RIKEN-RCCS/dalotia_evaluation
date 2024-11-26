aa3b73299782e9513578dec15fb617be0de69d50
Theresa Pollinger <theresa.pollinger@riken.jp>
2024-11-26 17:40:54 +0900

    mnist_cpp: slow but correct variant

--- benchmarks/mnist/mnist.cpp
+++ benchmarks/mnist/mnist.cpp
@@ -1,5 +1,6 @@
 #include <algorithm>
 #include <cassert>
+#include <cmath>
 #include <fstream>
 #include <iostream>
 
@@ -45,28 +46,381 @@ std::pmr::vector<float> read_mnist_scaled(std::string full_path) {
     if (file.is_open()) {
         int magic_number = 0;
         int number_of_images = 0;
-        int n_rows = 0;
-        int n_cols = 0;
         file.read((char *)&magic_number, sizeof(magic_number));
         magic_number = reverseEndianness(magic_number);
-        assert(magic_number == 2051 || magic_number == 2049);
-        file.read((char *)&number_of_images, sizeof(number_of_images));
-        number_of_images = reverseEndianness(number_of_images);
-        assert(number_of_images == 10000);
-        file.read((char *)&n_rows, sizeof(n_rows));
-        n_rows = reverseEndianness(n_rows);
-        assert(n_rows == 28);
-        file.read((char *)&n_cols, sizeof(n_cols));
-        n_cols = reverseEndianness(n_cols);
-        assert(n_cols == 28);
-        vector_of_images.resize(number_of_images * n_rows * n_cols);
-        file.read((char *)vector_of_images.data(), vector_of_images.size());
-        std::transform(vector_of_images.begin(), vector_of_images.end(),
-                       std::back_inserter(vector_of_images_scaled),
-                       [](uint8_t x) { return x / 255.0; });
+        if (full_path.find("images") != std::string::npos) {
+            std::cout << "Reading images" << std::endl;
+            int n_rows = 0;
+            int n_cols = 0;
+            assert(magic_number == 2051);
+            file.read((char *)&number_of_images, sizeof(number_of_images));
+            number_of_images = reverseEndianness(number_of_images);
+            assert(number_of_images == 10000);
+            file.read((char *)&n_rows, sizeof(n_rows));
+            n_rows = reverseEndianness(n_rows);
+            assert(n_rows == 28);
+            file.read((char *)&n_cols, sizeof(n_cols));
+            n_cols = reverseEndianness(n_cols);
+            assert(n_cols == 28);
+            vector_of_images.resize(number_of_images * n_rows * n_cols);
+            file.read((char *)vector_of_images.data(), vector_of_images.size());
+            std::transform(vector_of_images.begin(), vector_of_images.end(),
+                           std::back_inserter(vector_of_images_scaled),
+                           [](uint8_t x) { return x / 255.0; });
+        } else {
+            std::cout << "Reading labels" << std::endl;
+            assert(magic_number == 2049);
+            file.read((char *)&number_of_images, sizeof(number_of_images));
+            number_of_images = reverseEndianness(number_of_images);
+            assert(number_of_images == 10000);
+            vector_of_images.resize(number_of_images);
+            file.read((char *)vector_of_images.data(), vector_of_images.size());
+            std::transform(vector_of_images.begin(), vector_of_images.end(),
+                           std::back_inserter(vector_of_images_scaled),
+                           [](uint8_t x) { return x; });
+        }
         file.close();
+    } else {
+        throw std::runtime_error("Cannot open file `" + full_path + "`");
     }
     return vector_of_images_scaled;
+}
+
+template <int dim>
+std::function<int(std::array<int, dim>)> get_tensor_indexer(
+    const std::array<int, dim> &extents) {
+    std::array<int, dim> strides;
+    std::exclusive_scan(extents.rbegin(), extents.rend(), strides.rbegin(), 1,
+                        std::multiplies<int>());
+    const std::array<int, dim> const_strides = strides;
+    return [const_strides, &extents](std::array<int, dim> indices) {
+#ifndef NDEBUG
+        for (int i = 0; i < dim; ++i) {
+            if (indices[i] < 0 || indices[i] >= extents[i]) {
+                std::cerr << "index " << i << " out of bounds: " << indices[i]
+                          << " not in [0, " << extents[i] << ")" << std::endl;
+            }
+            assert(indices[i] >= 0 && indices[i] < extents[i]);
+        }
+#endif  // NDEBUG
+        return std::inner_product(indices.begin(), indices.end(),
+                                  const_strides.begin(), 0);
+    };
+}
+
+template <typename ExtentsType>
+void print_first_formatted(const std::pmr::vector<float> &output,
+                           const ExtentsType &extents, std::ostream &os) {
+    for (int i = 0; i < 1; ++i) {  // extents[0]
+        os << "output for image " << i << ": " << std::endl;
+        for (int j = 0; j < extents[1]; ++j) {
+            for (int k = 0; k < extents[2]; ++k) {
+                os << output[0 + j * extents[2] + k] << ", ";
+            }
+            os << std::endl;
+        }
+        os << std::endl;
+    }
+}
+
+void assert_close(float a, float b, float tol = 1e-4) {
+#ifndef NDEBUG
+    if (std::abs(a - b) >= tol) {
+        std::cerr << "assertion failed: " << a << " != " << b << std::endl;
+    }
+    assert(std::abs(a - b) < tol);
+#endif  // NDEBUG
+}
+
+void run_inference_slow_loops(std::string filename) {
+    const auto [conv1_weight, conv1_bias] = test_load(filename, "conv1");
+    std::array<int, 4> conv1_weight_extents = {8, 1, 3, 3};
+    auto conv1_weight_indexer = get_tensor_indexer<4>(conv1_weight_extents);
+    auto [conv2_weight, conv2_bias] = test_load(filename, "conv2");
+    std::array<int, 4> conv2_weight_extents = {16, 8, 3, 3};
+    auto conv2_weight_indexer = get_tensor_indexer<4>(conv2_weight_extents);
+    auto [fc1_weight, fc1_bias] = test_load(filename, "fc1");
+    std::array<int, 2> fc1_weight_extents = {10, 784};
+    auto fc1_weight_indexer = get_tensor_indexer<2>(fc1_weight_extents);
+
+    // load the mnist test data // as in
+    // https://medium.com/@myringoleMLGOD/simple-convolutional-neural-network-cnn-for-dummies-in-pytorch-a-step-by-step-guide-6f4109f6df80
+    // too
+    std::string mnist_test_images_filename = "./t10k-images-idx3-ubyte";
+    std::string mnist_test_labels_filename = "./t10k-labels-idx1-ubyte";
+
+    auto images = read_mnist_scaled(mnist_test_images_filename);
+    // assert on the first non-0 value
+    assert_close(images[7 * 28 + 6], 0.3294);
+    auto labels = read_mnist_scaled(mnist_test_labels_filename);
+    auto total_num_images = images.size() / (28 * 28);
+    assert(total_num_images == labels.size());
+
+    std::pmr::vector<int> results(total_num_images);
+
+    // minibatching
+    constexpr size_t batch_size = 64;
+    auto num_batches = static_cast<int>(
+        std::ceil(total_num_images / static_cast<float>(batch_size)));
+    for (size_t batch_index = 0; batch_index < num_batches; ++batch_index) {
+        auto num_images_in_batch =
+            std::min(batch_size, total_num_images - batch_index * batch_size);
+        auto inum_images_in_batch = static_cast<int>(num_images_in_batch);
+        assert(inum_images_in_batch > 0);
+        std::cout << "batch index: " << batch_index << " / " << num_batches
+                  << " num images in batch: " << num_images_in_batch
+                  << std::endl;
+
+        // apply first convolution
+        // copy data to larger array for zero-padding at the edges
+        auto image_vector_padded =
+            std::pmr::vector<float>(num_images_in_batch * 30 * 30);
+        std::array<int, 3> image_padded_extents = {inum_images_in_batch, 30,
+                                                   30};
+        auto image_padded_indexer = get_tensor_indexer<3>(image_padded_extents);
+
+        for (int o = 0; o < inum_images_in_batch; ++o) {
+            for (int i = 0; i < 28; ++i) {
+                for (int j = 0; j < 28; ++j) {
+                    image_vector_padded[image_padded_indexer(
+                        {o, i + 1, j + 1})] =
+                        images[batch_index * (batch_size * 28 * 28) +
+                               o * (28 * 28) + i * 28 + j];
+                }
+            }
+        }
+
+        auto conv1_output =
+            std::pmr::vector<float>(num_images_in_batch * 8 * 28 * 28);
+        std::array<int, 4> conv1_output_extents = {inum_images_in_batch, 8, 28,
+                                                   28};
+        auto conv1_output_indexer = get_tensor_indexer<4>(conv1_output_extents);
+        for (int o = 0; o < inum_images_in_batch; ++o) {
+            for (int k = 0; k < 8; ++k) {
+                for (int i = 1; i < 29; ++i) {
+                    for (int j = 1; j < 29; ++j) {
+                        float value = 0;
+                        for (int m = 0; m < 3; ++m) {
+                            for (int n = 0; n < 3; ++n) {
+                                value +=
+                                    conv1_weight[conv1_weight_indexer(
+                                        {k, 0, m, n})] *
+                                    image_vector_padded[image_padded_indexer(
+                                        {o, i + m - 1, j + n - 1})];
+                            }
+                        }
+                        value += conv1_bias[k];
+                        if (value < 0.) {
+                            value = 0.;
+                        }
+                        conv1_output[conv1_output_indexer(
+                            {o, k, i - 1, j - 1})] = value;
+                    }
+                }
+            }
+        }
+
+        // apply max pooling
+        auto conv1_output_pooled =
+            std::pmr::vector<float>(num_images_in_batch * 8 * 14 * 14);
+        std::array<int, 4> conv1_output_pooled_extents = {inum_images_in_batch,
+                                                          8, 14, 14};
+        auto conv1_output_pooled_indexer =
+            get_tensor_indexer<4>(conv1_output_pooled_extents);
+        for (int o = 0; o < inum_images_in_batch; ++o) {
+            for (int k = 0; k < 8; ++k) {
+                for (int i = 0; i < 14; ++i) {
+                    for (int j = 0; j < 14; ++j) {
+                        float max_val = 0;
+                        for (int m = 0; m < 2; ++m) {
+                            for (int n = 0; n < 2; ++n) {
+                                max_val = std::max(
+                                    max_val,
+                                    conv1_output[conv1_output_indexer(
+                                        {o, k, 2 * i + m, 2 * j + n})]);
+                            }
+                        }
+                        conv1_output_pooled[conv1_output_pooled_indexer(
+                            {o, k, i, j})] = max_val;
+                    }
+                }
+            }
+        }
+
+        // print_first_formatted(conv1_output_pooled,
+        // conv1_output_pooled_extents,
+        //                       std::cout);
+
+        // apply second convolution
+        // copy data to larger array for zero-padding at the edges
+        auto feature_vector_padded =
+            std::pmr::vector<float>(num_images_in_batch * 8 * 16 * 16);
+        std::array<int, 4> feature_padded_extents = {inum_images_in_batch, 8,
+                                                     16, 16};
+        auto feature_padded_indexer =
+            get_tensor_indexer<4>(feature_padded_extents);
+        for (int o = 0; o < inum_images_in_batch; ++o) {
+            for (int k = 0; k < 8; ++k) {
+                for (int i = 1; i < 15; ++i) {
+                    for (int j = 1; j < 15; ++j) {
+                        feature_vector_padded[feature_padded_indexer(
+                            {o, k, i, j})] =
+                            conv1_output_pooled[conv1_output_pooled_indexer(
+                                {o, k, i - 1, j - 1})];
+                    }
+                }
+            }
+        }
+
+        auto conv2_output =
+            std::pmr::vector<float>(num_images_in_batch * 16 * 14 * 14);
+        std::array<int, 4> conv2_output_extents = {inum_images_in_batch, 16, 14,
+                                                   14};
+        auto conv2_output_indexer = get_tensor_indexer<4>(conv2_output_extents);
+        for (int o = 0; o < inum_images_in_batch; ++o) {
+            for (int k = 0; k < 16; ++k) {
+                for (int i = 1; i < 15; ++i) {
+                    for (int j = 1; j < 15; ++j) {
+                        float value = 0;
+                        for (int l = 0; l < 8; ++l) {
+                            for (int m = 0; m < 3; ++m) {
+                                for (int n = 0; n < 3; ++n) {
+                                    value +=
+                                        conv2_weight[conv2_weight_indexer(
+                                            {k, l, m, n})] *
+                                        feature_vector_padded
+                                            [feature_padded_indexer(
+                                                {o, l, i + m - 1, j + n - 1})];
+                                }
+                            }
+                        }
+                        value += conv2_bias[k];
+                        if (value < 0.) {
+                            value = 0.;
+                        }
+                        conv2_output[conv2_output_indexer(
+                            {o, k, i - 1, j - 1})] = value;
+                    }
+                }
+            }
+        }
+
+        // apply max pooling
+        auto conv2_output_pooled =
+            std::pmr::vector<float>(num_images_in_batch * 16 * 7 * 7);
+        std::array<int, 4> conv2_output_pooled_extents = {inum_images_in_batch,
+                                                          16, 7, 7};
+        auto conv2_output_pooled_indexer =
+            get_tensor_indexer<4>(conv2_output_pooled_extents);
+        for (int o = 0; o < inum_images_in_batch; ++o) {
+            for (int i = 0; i < 7; ++i) {
+                for (int j = 0; j < 7; ++j) {
+                    for (int k = 0; k < 16; ++k) {
+                        float max_val = 0;
+                        for (int m = 0; m < 2; ++m) {
+                            for (int n = 0; n < 2; ++n) {
+                                max_val = std::max(
+                                    max_val,
+                                    conv2_output[conv2_output_indexer(
+                                        {o, k, 2 * i + m, 2 * j + n})]);
+                            }
+                        }
+                        conv2_output_pooled[conv2_output_pooled_indexer(
+                            {o, k, i, j})] = max_val;
+                    }
+                }
+            }
+        }
+
+        // apply dense layer
+        auto fc1_output = std::pmr::vector<float>(num_images_in_batch * 10);
+        std::array<int, 2> fc1_output_extents = {inum_images_in_batch, 10};
+        auto fc1_output_indexer = get_tensor_indexer<2>(fc1_output_extents);
+        std::array<int, 2> conv2_output_flat_extents = {inum_images_in_batch,
+                                                        16 * 7 * 7};
+        auto conv2_output_flat_indexer =
+            get_tensor_indexer<2>(conv2_output_flat_extents);
+        for (int o = 0; o < inum_images_in_batch; ++o) {
+            for (int k = 0; k < 10; ++k) {
+                float value = 0.;
+                for (int l = 0; l < 16 * 7 * 7; ++l) {
+                    value +=
+                        fc1_weight[fc1_weight_indexer({k, l})] *
+                        conv2_output_pooled[conv2_output_flat_indexer({o, l})];
+                }
+                value += fc1_bias[k];
+                fc1_output[fc1_output_indexer({o, k})] = value;
+            }
+        }
+
+        // apply softmax and compare to labels
+        for (int o = 0; o < inum_images_in_batch; ++o) {
+            auto result = std::max_element(fc1_output.begin() + o * 10,
+                                           fc1_output.begin() + (o + 1) * 10) -
+                          (fc1_output.begin() + o * 10);
+            results[batch_index * batch_size + o] = result;
+        }
+
+#ifndef NDEBUG
+        if (batch_index == 0) {  // compare to Python results
+            assert_close(conv1_output[0], 0.1796);
+            assert_close(conv1_output[conv1_output_indexer({0, 7, 27, 27})],
+                         0.6550);
+            assert_close(conv1_output_pooled[0], 0.1796);
+            assert_close(conv1_output_pooled[conv1_output_pooled_indexer(
+                             {0, 7, 13, 13})],
+                         0.6550);
+            assert_close(conv2_output[0], 0.4063);
+            assert_close(conv2_output[conv2_output_indexer({0, 1, 13, 13})],
+                         0.1789);
+            assert_close(conv2_output[conv2_output_indexer({0, 13, 0, 0})],
+                         0.1906);
+            assert_close(conv2_output[conv2_output_indexer({0, 15, 13, 13})],
+                         0.0);
+            assert_close(conv2_output_pooled[0], 0.4063);
+            assert_close(
+                conv2_output_pooled[conv2_output_pooled_indexer({0, 1, 0, 0})],
+                0.04935);
+            assert_close(
+                conv2_output_pooled[conv2_output_pooled_indexer({0, 1, 6, 6})],
+                0.49008);
+            assert_close(
+                conv2_output_pooled[conv2_output_pooled_indexer({0, 13, 0, 0})],
+                0.1906);
+            assert_close(
+                conv2_output_pooled[conv2_output_pooled_indexer({0, 15, 6, 6})],
+                0.0);
+
+            assert_close(conv2_output_pooled[0], 0.40625, 1e-5);
+            assert_close(conv2_output_pooled[1], 0.0);
+            assert_close(conv2_output_pooled[8], 6.2347);
+            assert_close(conv2_output_pooled[783], 0.0);
+            assert_close(fc1_output[0], -80.9247);
+            assert_close(fc1_output[1], -34.6855);
+            assert_close(fc1_output[2], -31.1533);
+            assert_close(fc1_output[3], -12.5210);
+            assert_close(fc1_output[4], -44.1289);
+            assert_close(fc1_output[5], -40.3522);
+            assert_close(fc1_output[6], -139.7097);
+            assert_close(fc1_output[7], 38.1572);
+            assert_close(fc1_output[8], -47.0220);
+            assert_close(fc1_output[9], -7.9544);
+        }
+#endif  // NDEBUG
+    }
+
+    int num_correct = 0;
+    int num_incorrect = 0;
+    for (int o = 0; o < total_num_images; ++o) {
+        if (results[o] == labels[o]) {
+            num_correct++;
+        } else {
+            num_incorrect++;
+        }
+    }
+    assert(num_correct + num_incorrect == total_num_images);
+    assert(num_correct == 9862);
+    std::cout << "Got " << num_correct << "/" << total_num_images << std::endl;
 }
 
 #ifdef DALOTIA_E_WITH_BOOST_MULTI
@@ -395,6 +749,7 @@ int main(int, char **) {
 #ifdef DALOTIA_E_WITH_BOOST_MULTI
     run_inference_boost_multi(filename);
 #else
+    run_inference_slow_loops(filename);
     throw std::runtime_error("DALOTIA_E_WITH_BOOST_MULTI not defined");
 #endif  // DALOTIA_E_WITH_BOOST_MULTI
     return 0;
