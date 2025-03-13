f648ba58f94b750a88e7b6ea88b3b0a9a03adc0d
Theresa Pollinger <theresa.pollinger@riken.jp>
2025-03-13 10:30:49 +0900

    tools: extend extract script to roofline vals

--- tools/extract_avg_values.py
+++ tools/extract_avg_values.py
@@ -23,30 +23,43 @@ if __name__ == "__main__":
     print(input_sizes)
 
     # later: manually map to column names:
-    # columns = ['input_size', 'deeprl_dnnl', 'deeprl_libtorch', 'deeprl_pytorch_jit',
-    #         'subgridles_libtorch', 'subgridles_cpp', 'subgridles_fortran',
-    #         'subgridles_pytorch_jit']
+    # input-size, deeprleddy-gcc-open-dnnl, deeprleddy-gcc-open-libtorch, deeprleddy-gcc-open-pytorch,
+    # subgridles-gcc-open-cpp, subgridles-gcc-open-fortran,
+    # deeprleddy-gcc-mkl-dnnl, subgridles-gcc-open-libtorch, subgridles-gcc-open-pytorch, 
+    # subgridles-gcc-mkl-cpp,subgridles-gcc-mkl-fortran
     if args.type == "runtime":
         # find all occurrences of On average: followed by a number,
         # with the input length listed between them in the file
         # cf. https://docs.python.org/3/library/re.html#simulating-scanf
         results = re.findall(r"On average:\s*(\S+)\s*s", contents)
-        assert len(results) > 0
         results = [float(result) for result in results]
     elif args.type == "memory":
         # the results are listed on a single line,
         # and input lengths are listed on some line above
-        results = re.findall(r"\n(\d+)\n", contents)
-        assert len(results) > 0
+        results = re.findall(r"\n(\d+)", contents)
         results = [int(result) for result in results]
     elif args.type == "energy":
         results = re.findall(r"\|\s*Energy \[J\]\s*\|\s*(\d+\.?\d*)\s*\|", contents)
-        assert len(results) > 0
-        print(len(results), results)
         num_repetitions = 1000
         results = [float(result) / num_repetitions for result in results]
-    print(results)
+    elif args.type == "flops":
+        results = re.findall(r"\|\s*SP \[MFLOP/s\]\s*\|\s*(\d+\.?\d*)\s*\|", contents)
+        results = [float(result) for result in results]
+    elif args.type == "intensity":
+        results = re.findall(r"\|\s*Operational intensity \[FLOP/Byte\]\s*\|\s*(\d+\.?\d*)\s*\|", contents)
+        results = [float(result) for result in results]
+    assert len(results) > 0    
+    print(len(input_sizes), len(results), results)
+
     assert len(results) % len(input_sizes) == 0
+    # if the sizes don't match, find the failed runs and add this to the output file:
+    """
+    On average: 0.0 s
+    | Energy [J] | 0.0 |
+    | SP [MFLOP/s] | 0.0 |
+    | Operational intensity [FLOP/Byte] | 0.0 |
+    """
+
     num_variants = len(results) // len(input_sizes)
     columns = ["input_size"] + [str(i + 1) for i in range(num_variants)]
     data = []
